1391b947c72843994ff90196b5343d24f78ec3dd
glULTRA <7amakurd2002@gmail.com>
2022-05-31 11:42:49 +0300

    A Great example for learning

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,109 @@
 import 'package:flutter/material.dart';
 
-void main(List<String> args) {
-  runApp(const MyApp());
+dynamic main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class _MyAppState extends State<MyApp> {
+  int counter = 0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.grey[900],
         appBar: AppBar(
-          backgroundColor: Colors.lightBlue,
-          title: const Text("Tasks"),
+          title: const Text("Ninja Card ID"),
+          centerTitle: true,
+          backgroundColor: Colors.grey[850],
+          elevation: 0.0,
         ),
-        drawer: const Drawer(
-          backgroundColor: Colors.blueAccent,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => setState(() {
+            counter++;
+          }),
+          child: const Icon(Icons.add),
         ),
-        body: Container(
-          decoration: const BoxDecoration(
-            color: Colors.cyanAccent,
-            image: DecorationImage(
-              fit: BoxFit.fitWidth,
-              image: AssetImage(
-                'images/dash-fainting.gif',
+        body: Padding(
+          padding: const EdgeInsets.all(35),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const Center(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage("images/man.jpg"),
+                  radius: 70.0,
+                ),
               ),
-            ),
-            borderRadius: BorderRadius.only(
-              bottomRight: Radius.circular(100),
-              topRight: Radius.circular(100),
-              bottomLeft: Radius.circular(20),
-              topLeft: Radius.circular(20),
-            ),
-          ),
-          margin: const EdgeInsets.only(
-            left: 40,
-            bottom: 400,
-            top: 20,
-            right: 40,
-          ),
-          width: 400,
-          height: 400,
-          child: const Text(
-            "Hello",
-            style: TextStyle(
-              fontSize: 35,
-              color: Colors.deepPurpleAccent,
-            ),
-            textAlign: TextAlign.center,
+              Divider(
+                height: 90.0,
+                color: Colors.grey[800],
+              ),
+              const Text(
+                'Name',
+                style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0,
+                  fontSize: 20,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                'Mohammed Khalid',
+                style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2,
+                  fontSize: 27,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Current Level',
+                style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0,
+                  fontSize: 20,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                '$counter',
+                style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2,
+                  fontSize: 27,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.email,
+                    color: Colors.grey[400],
+                  ),
+                  const SizedBox(width: 15),
+                  Text(
+                    "7amakurd2002@gmail.com",
+                    style: TextStyle(
+                      color: Colors.grey[400],
+                      letterSpacing: 2,
+                      fontSize: 17,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
-        backgroundColor: Colors.blue,
       ),
     );
   }
